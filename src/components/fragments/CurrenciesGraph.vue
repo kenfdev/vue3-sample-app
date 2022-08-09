747f0a3f55df4d85bb4f228c1e1c542d2da2ff24
Ken Fukuyama <kenfdev@gmail.com>
2022-08-10 08:41:37 +0900

    add feature to change date

--- src/components/fragments/CurrenciesGraph.vue
+++ src/components/fragments/CurrenciesGraph.vue
@@ -4,6 +4,10 @@
       <v-col cols="2">
         <v-checkbox v-model="isCurrency1Enabled" label="Currency 1"></v-checkbox>
         <v-checkbox v-model="isCurrency2Enabled" label="Currency 2"></v-checkbox>
+        <v-text-field label="Start Date" placeholder="Start Date" :model-value="startDate"
+          @update:model-value="handleStartDate"></v-text-field>
+        <v-text-field label="End Date" placeholder="End Date" :model-value="endDate"
+          @update:model-value="handleEndDate"></v-text-field>
       </v-col>
       <v-col cols="10">
         <apexchart width="100%" type="line" :options="options" :series="series" />
@@ -14,10 +18,27 @@
 
 <script setup lang="ts">
 import { CurrencyData } from '@/shared/models/currency';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import dayjs from 'dayjs';
 import { ref, watch } from 'vue';
 import Apexchart from 'vue3-apexcharts';
 
-const { currencies } = defineProps<{ currencies: CurrencyData[] | undefined }>();
+dayjs.extend(customParseFormat);
+
+const emit = defineEmits<{ (e: 'changeStartDate', val: string): void, (e: 'changeEndDate', val: string): void }>();
+const { currencies, startDate, endDate } = defineProps<{ currencies: CurrencyData[] | undefined, startDate: string | undefined, endDate: string }>();
+
+const handleStartDate = (value: string) => {
+  console.log('value', value)
+  if (dayjs(value, 'YYYY/MM/DD', true).isValid()) {
+    emit('changeStartDate', value)
+  }
+}
+const handleEndDate = (value: string) => {
+  if (dayjs(value, 'YYYY/MM/DD', true).isValid()) {
+    emit('changeEndDate', value)
+  }
+}
 
 function createSeries(currencies: CurrencyData[] | undefined, isCurrency1Enabled: boolean, isCurrency2Enabled: boolean) {
   const currency1 = currencies?.map(v => [v.timestamp, v.currency1])
--- src/pages/currencies/CurrenciesPage.vue
+++ src/pages/currencies/CurrenciesPage.vue
@@ -2,7 +2,9 @@
   <default-layout>
     <spinner v-if="isLoading"></spinner>
     <div v-else class="w-100">
-      <currencies-graph :currencies="currencies"></currencies-graph>
+      <currencies-graph :currencies="currencies" :start-date="input.start" :end-date="input.end"
+        @change-start-date="handleChangeStartDate" @change-end-date="handleChangeEndDate">
+      </currencies-graph>
     </div>
   </default-layout>
 </template>
@@ -12,8 +14,13 @@ import Spinner from '@/components/essentials/Spinner.vue';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import { useCurrencies } from '@/composables/currencies/useCurrencies';
 import CurrenciesGraph from '@/components/fragments/CurrenciesGraph.vue';
+import { reactive } from 'vue';
 
-const { currencies, isLoading, } = useCurrencies({});
+const input = reactive({ start: '', end: '' })
+const { currencies, isLoading, } = useCurrencies(input);
+
+const handleChangeStartDate = (v: string) => input.start = v;
+const handleChangeEndDate = (v: string) => input.end = v;
 
 </script>
 
