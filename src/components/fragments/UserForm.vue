f5c1dc87eebbee4d205141d5de4e6fbbbae9582c
Ken Fukuyama <kenfdev@gmail.com>
2022-08-12 13:13:02 +0900

    add vee-validate and zod for form validation

--- src/components/fragments/UserForm.vue
+++ src/components/fragments/UserForm.vue
@@ -4,24 +4,24 @@
       <span class="text-h5">User Profile</span>
     </v-card-title>
     <v-card-text>
-      <v-form>
+      <v-form ref="form">
         <v-container>
           <v-row>
             <v-col cols="12">
-              <v-text-field label="Username*" required v-model="username"></v-text-field>
+              <v-text-field label="Username*" v-model="username" :error-messages="errors.username"></v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-text-field label="First Name*" required v-model="firstName"></v-text-field>
+              <v-text-field label="First Name*" v-model="firstName" :error-messages="errors.firstName"></v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-text-field label="Last Name*" required v-model="lastName">
+              <v-text-field label="Last Name*" v-model="lastName" :error-messages="errors.lastName">
               </v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-text-field label="Email*" required v-model="email"></v-text-field>
+              <v-text-field label="Email*" v-model="email" :error-messages="errors.email"></v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-text-field label="Phone*" type="tel" required v-model="phone"></v-text-field>
+              <v-text-field label="Phone" type="tel" v-model="phone" :error-messages="errors.phone"></v-text-field>
             </v-col>
           </v-row>
         </v-container>
@@ -33,7 +33,7 @@
       <v-btn color="blue-darken-1" text @click="() => $emit('cancel')">
         Close
       </v-btn>
-      <v-btn color="blue-darken-1" text @click="onSubmit">
+      <v-btn color="blue-darken-1" text @click="onSave">
         Save
       </v-btn>
     </v-card-actions>
@@ -43,18 +43,44 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { UserCreateInput } from '@/api/users';
+import { useForm, useField } from 'vee-validate';
+import * as zod from 'zod';
+import { toFormValidator } from '@vee-validate/zod';
 
-const username = ref('')
-const firstName = ref('')
-const lastName = ref('')
-const email = ref('')
-const phone = ref('')
+interface UserForm {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+}
+
+const { errors, validate } = useForm<UserForm>({
+  validationSchema: toFormValidator(zod.object({
+    username: zod.string({ required_error: 'Username is required' }).min(6, 'Username must be longer than 6 characters'),
+    firstName: zod.string({ required_error: 'First Name is required' }),
+    lastName: zod.string({ required_error: 'Last Name is required' }),
+    email: zod.string({ required_error: 'Email is required' }).email('Enter a valid Email'),
+    phone: zod.string().optional()
+  }))
+});
+
+const form = ref<HTMLFormElement | null>(null)
+
+const { value: username } = useField<string>('username');
+const { value: firstName } = useField<string>('firstName');
+const { value: lastName } = useField<string>('lastName');
+const { value: email } = useField<string>('email');
+const { value: phone } = useField<string>('phone');
 
 const emit = defineEmits<{ (e: 'cancel'): void, (e: 'submit', user: UserCreateInput): void }>()
 
-function onSubmit() {
+const onSave = async () => {
+  const result = await validate()
+  console.log('result', result);
+  if (!result.valid) return
+
   const userInput = { username: username.value, firstName: firstName.value, lastName: lastName.value, email: email.value, phone: phone.value };
-  console.log(userInput)
   emit('submit', userInput)
 }
 
